493949c6446bc1e46c5e2006ba74049c47000398
k-ming <keiramingj@gmail.com>
2021-04-15 22:51:50 +0900

    done :)

--- js/function.js
+++ js/function.js
@@ -7,6 +7,7 @@ const api ={
     key: "2dce14b5cb89113de2ab27e458030969",
     base: "https://api.openweathermap.org/data/2.5/"
 }
+
 //Get Current location
 function findLocation() {
   if (navigator.geolocation) { 
@@ -16,18 +17,6 @@ function findLocation() {
     }
 }
 findLocation();
-
-
-
-
-//Get Time
-var today = new Date();
-var hours = today.getHours();
-var mins = today.getMinutes();
-
-hours > 11 ? timeDisplay.innerText = `${hours} : ${mins} pm` : 
-timeDisplay.innerText = `0${hours} : ${mins} am`
-
 
 //Get weather
 function getResults(position){
@@ -47,4 +36,71 @@ function getWeather(weather){
 
 
 
+//Get Time
+var today = new Date();
+var hours = today.getHours();
+var mins = today.getMinutes();
 
+hours > 11 ? timeDisplay.innerText = `${hours} : ${mins} pm` : 
+timeDisplay.innerText = `0${hours} : ${mins} am`
+
+
+//build todolist
+const todobtn = document.querySelector('.todo');
+const todoCon = document.querySelector('.todobox');
+const Input = document.querySelector('.todotext');
+const todobox = document.querySelector('.todolist');
+const form = document.querySelector('form');
+
+todobtn.addEventListener('click', (e) => {
+  e.preventDefault;
+  todoCon.classList.toggle('active');
+
+});
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = Input.value;
+  createTodolist(currentValue);
+  Input.value = "";
+
+}
+
+//To do list UI
+function createTodolist(text){
+  const li = document.createElement('li');
+  li.classList.add('list-line');
+  todobox.appendChild(li);
+  li.innerText = text;
+
+  const checkbox = document.createElement('span');
+  checkbox.classList.add('checkbox')
+  checkbox.innerHTML = `<i class="fas fa-check"></i>`;
+
+  const delBtn = document.createElement('p');
+  delBtn.classList.add('delete');
+  delBtn.innerHTML = `<i class="far fa-trash-alt"></i>`;
+
+  li.prepend(checkbox);
+  li.append(delBtn);
+
+  delBtn.addEventListener('click', deltodo);
+  checkbox.addEventListener('click', doneEvent);
+  
+}
+
+//delete todo list
+function deltodo(event){
+  const btn = event.target;
+  const listLine = btn.parentNode.parentNode;
+  todobox.removeChild(listLine);
+
+}
+
+function doneEvent(event){
+  const checkBtn = event.target;
+  const listLine = checkBtn.parentNode;
+  listLine.classList.toggle('active');  
+}
